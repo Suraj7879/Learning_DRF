5fe51087fbfed5be619c1b3435e1454250bcfd9d
Suraj Singh <surajsingh7346@gmail.com>
2021-05-31 22:57:10 +0530

    Created CheckList api

---
+++ core/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from core.models import CheckList
+
+from core.serializers import CheckListSerializer
+# Create your views here.
+
+
+class TestAPIView(APIView):
+      def get(self, request, format=None):
+            return Response({'name': 'Suraj from Chennai'})
+
+class CheckListsAPIView(APIView):
+      serializer_class = CheckListSerializer
+      def get(self, request, format=None):
+            data = CheckList.objects.all()
+
+            serializer = self.serializer_class(data, many=True)
+            serialized_data = serializer.data
+
+            return Response(serialized_data)
+
